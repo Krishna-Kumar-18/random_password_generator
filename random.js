320b73a37bb4b96b6a2f1b5dd80050fff77030b3
Krishna Kumar <krishnakumar750514@gmail.com>
2024-04-04 00:54:47 +0530

    file complete

---
+++ random.js
@@ -0,0 +1,28 @@
+const passwordBox = document.querySelector("#Password");
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const specialSymbol = "[@_!#$%^&*()<>?/|}{~:]";
+const allChars = upperCase + lowerCase + number + specialSymbol;
+
+const length = 12;
+
+function generatePassword() {
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += specialSymbol[Math.floor(Math.random() * specialSymbol.length)];
+
+    while(password.length < length)
+    {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select();
+    document.execCommand("copy");
+}
